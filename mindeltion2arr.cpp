563e647bde437a34192d00aa02c7873137e989ae
Umang <umangsinghal2007@gmail.com>
2026-09-01 22:07:33 +0530

    Refactor minOperations for GCD and divisor counting
    Refactor minOperations to use GCD and find minimum valid divisor count.

--- mindeltion2arr.cpp
+++ mindeltion2arr.cpp
@@ -1,51 +1,83 @@
+// // class Solution {
+// // public:
+// //     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
+// //         sort(nums.begin(),nums.end());
+// //         int result;
+        
+// //         for(int i  = 0 ; i < nums.size() ; i++){
+// //             bool flag = true;
+// //             for(int j = 0 ; j < numsDivide.size() ; j++){
+// //                 if(numsDivide[j]%nums[i]!=0){
+// //                     flag = false;
+// //                     break;
+// //                 }
+                
+                
+// //             }
+// //             if(flag==true){
+// //                 return i;
+// //             }
+// //         }
+// //         return -1;
+// //     }
+// // };
+
+
+
+
 // class Solution {
 // public:
 //     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
-//         sort(nums.begin(),nums.end());
-//         int result;
         
-//         for(int i  = 0 ; i < nums.size() ; i++){
-//             bool flag = true;
-//             for(int j = 0 ; j < numsDivide.size() ; j++){
-//                 if(numsDivide[j]%nums[i]!=0){
-//                     flag = false;
-//                     break;
-//                 }
-                
-                
-//             }
-//             if(flag==true){
+//         // Find GCD of all elements in numsDivide
+//         int gcd = numsDivide[0];
+
+//         for (int i = 1; i < numsDivide.size(); i++) {
+//             gcd = __gcd(gcd, numsDivide[i]);
+//         }
+
+//         // Sort nums so we get the smallest valid divisor
+//         sort(nums.begin(), nums.end());
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (gcd % nums[i] == 0) {
 //                 return i;
 //             }
 //         }
+
 //         return -1;
 //     }
 // };
 
-
-
-
+```cpp
 class Solution {
 public:
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
-        
-        // Find GCD of all elements in numsDivide
-        int gcd = numsDivide[0];
+        int g = numsDivide[0];
 
-        for (int i = 1; i < numsDivide.size(); i++) {
-            gcd = __gcd(gcd, numsDivide[i]);
+        for (int x : numsDivide)
+            g = gcd(g, x);
+
+        int mn = INT_MAX;
+
+        for (int x : nums) {
+            if (g % x == 0)
+                mn = min(mn, x);
         }
 
-        // Sort nums so we get the smallest valid divisor
-        sort(nums.begin(), nums.end());
+        if (mn == INT_MAX)
+            return -1;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (gcd % nums[i] == 0) {
-                return i;
-            }
+        int count = 0;
+
+        for (int x : nums) {
+            if (x < mn)
+                count++;
         }
 
-        return -1;
+        return count;
     }
 };
+```
+
 
