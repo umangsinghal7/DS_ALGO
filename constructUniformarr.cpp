88184a7d203a3a6637a33a6187b59038fa81d04b
Umang <umangsinghal2007@gmail.com>
2026-09-02 21:42:03 +0530

    Add uniformArray method to Solution class
    Implement uniformArray method to check array uniformity based on odd and even numbers.

---
+++ constructUniformarr.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+          // It is always possible to make the array all odd with at least 1 odd number since even - odd = odd, if there are no odds the array is already all even so it is always possible, the answer is just return true
+
+ //
+    return true;
+    }
+};
