304ca33d26d17deaccb836013322328ae72a536d
Umang <umangsinghal2007@gmail.com>
2026-09-03 22:37:59 +0530

    wtff me

---
+++ constructarr2.cpp
@@ -0,0 +1,16 @@
+// class Solution {
+// public:
+//     bool uniformArray(vector<int>& nums1) {
+//         bool flag = true;
+//         sort(nums1.begin(),nums1.end());
+//         for(int i = 0 ; i <nums1.size() ; i++ ){
+//             if(nums1[i]%2==0){
+
+//             }
+//         }      
+        
+//         if(nums1[0]%2==0){
+//             flag =false;
+//         }
+//     }
+// };
