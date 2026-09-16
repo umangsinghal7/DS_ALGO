65d23510feb2588a4e363049ae3bc312a01b90b3
Umang <umangsinghal2007@gmail.com>
2026-09-16 10:53:32 +0530

    Implement N-Queens solution with backtracking

---
+++ N_queenscount.cpp
@@ -0,0 +1,64 @@
+class Solution {
+public:
+    bool isSafe(vector<string>& board, int row, int col, int n) {
+
+        // Check column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+
+        // Check upper-left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        // Check upper-right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    void solve(int row,int &count , int n, vector<string>& board,
+        vector<vector<string>>& ans) {
+
+        // All queens placed
+        if (row == n) {
+            ans.push_back(board);
+            count++;
+            return;
+        }
+
+        // Try every column
+        for (int col = 0; col < n; col++) {
+
+            if (isSafe(board, row, col, n)) {
+
+                // Place queen
+                board[row][col] = 'Q';
+
+                // Go to next row
+                solve(row + 1,count, n, board, ans);
+
+                // Remove queen - BACKTRACK
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    int totalNQueens(int n) {
+
+        vector<vector<string>> ans;
+        int count = 0;
+
+        vector<string> board(n, string(n, '.'));
+
+        solve(0,count, n, board, ans);
+
+        return count;
+    }
+}; 
